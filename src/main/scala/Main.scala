6ab5f75336598ba6613bd51aa768dfa307853df8
Aybrl <contact@aybrl.com>
2022-11-05 21:07:10 +0100

    save data to hive

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,7 +2,6 @@ import sparkjobs.proto.LoadFromHDFS
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println(s"STARTED ${"!"*50}")
     LoadFromHDFS.loadFromHDFS
   }
 }
--- src/main/scala/sparkjobs/proto/LoadFromHDFS.scala
+++ src/main/scala/sparkjobs/proto/LoadFromHDFS.scala
@@ -9,21 +9,32 @@ object LoadFromHDFS {
 
   def loadFromHDFS: Unit = {
 
+    // log something
+    println(s"${"!" * 25} START ${"!" * 25}")
+
+    // Init spark session
     val sparkSession = SparkSession
       .builder
       .master(sparkHost)
       .appName("Spark CSV Reader")
       .getOrCreate()
 
+    // Load data from HDFS
     val df = sparkSession.read
       .format("csv")
       .option("header", "true")
       .option("mode", "DROPMALFORMED")
       .load(hdfsHost)
 
+    // PoC, show dataframe
     df.show(10)
 
-    println(s"FINISH ${"!"*50}")
+    // Save data to hive
+    df.createOrReplaceTempView("titanicTable")
+    sparkSession.sql("create table titanic_table as select * from titanicTable")
+
+    // log something :)
+    println(s"${"!"*25} FINISH ${"!"*25}")
 
 
   }
